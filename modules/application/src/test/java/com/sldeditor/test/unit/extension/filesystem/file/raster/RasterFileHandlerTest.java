4d016f151bae114e3e7c7e9a619c772bec5bd9ce
Rob Ward <robert.ward@scisys.co.uk>
2016-06-28 16:12:55 +0100

    Add more unit tests

---
+++ modules/application/src/test/java/com/sldeditor/test/unit/extension/filesystem/file/raster/RasterFileHandlerTest.java
@@ -0,0 +1,148 @@
+/*
+ * SLD Editor - The Open Source Java SLD Editor
+ *
+ * Copyright (C) 2016, SCISYS UK Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.sldeditor.test.unit.extension.filesystem.file.raster;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.sldeditor.common.SLDDataInterface;
+import com.sldeditor.common.data.SLDData;
+import com.sldeditor.common.data.StyleWrapper;
+import com.sldeditor.datasource.extension.filesystem.node.file.FileTreeNode;
+import com.sldeditor.extension.filesystem.file.raster.RasterFileHandler;
+
+/**
+ * Unit test for RasterFileHandler class.
+ * <p>{@link com.sldeditor.extension.filesystem.file.raster.RasterFileHandler}
+ * 
+ * @author Robert Ward (SCISYS)
+ *
+ */
+public class RasterFileHandlerTest {
+
+    /**
+     * Test method for {@link com.sldeditor.extension.filesystem.file.raster.RasterFileHandler#getFileExtensionList()}.
+     */
+    @Test
+    public void testGetFileExtension() {
+        assertEquals(Arrays.asList("tif", "jpg", "gif", "tiff"), new RasterFileHandler().getFileExtensionList());
+    }
+
+    /**
+     * Test method for {@link com.sldeditor.extension.filesystem.file.raster.RasterFileHandler#isDataSource()}.
+     */
+    @Test
+    public void testIsDataSource() {
+        assertTrue(new RasterFileHandler().isDataSource());
+    }
+
+    /**
+     * Test method for {@link com.sldeditor.extension.filesystem.file.raster.RasterFileHandler#populate(com.sldeditor.common.filesystem.FileSystemInterface, javax.swing.tree.DefaultTreeModel, com.sldeditor.datasource.extension.filesystem.node.file.FileTreeNode)}.
+     */
+    @Test
+    public void testPopulate() {
+        assertFalse(new RasterFileHandler().populate(null, null, null));
+    }
+
+    /**
+     * Single file
+     * 
+     * Test method for {@link com.sldeditor.extension.filesystem.file.raster.RasterFileHandler#getSLDContents(com.sldeditor.common.NodeInterface)}.
+     */
+    @Test
+    public void testGetSLDContentsFile() {
+        assertNull(new RasterFileHandler().getSLDContents(null));
+
+        URL url = RasterFileHandlerTest.class.getResource("/point/sld");
+
+        File parent = null;
+        try {
+            parent = new File(url.toURI());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+
+        try {
+            FileTreeNode fileTreeNode = new FileTreeNode(parent, "point_attribute.sld");
+
+            RasterFileHandler handler = new RasterFileHandler();
+
+            List<SLDDataInterface> sldDataList = handler.getSLDContents(fileTreeNode);
+
+            assertNull(sldDataList);
+
+            // Try with valid vector file
+            FileTreeNode fileTreeNode2 = new FileTreeNode(parent, "point_attribute.tif");
+
+            sldDataList = handler.getSLDContents(fileTreeNode2);
+
+            assertTrue(sldDataList.isEmpty());
+
+            // Changes where the file is to be saved to
+            File saveFile = File.createTempFile("test", ".sld");
+            saveFile.deleteOnExit();
+
+            SLDData sldData = new SLDData(null, "");
+
+            sldData.setSLDFile(saveFile);
+
+            assertFalse(handler.save(null));
+            assertFalse(handler.save(sldData));
+        } catch (SecurityException e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+    }
+
+    /**
+     * Supply a folder name and retrieve all the sld files in it
+     * 
+     * Test method for {@link com.sldeditor.extension.filesystem.file.raster.RasterFileHandler#getSLDContents(com.sldeditor.common.NodeInterface)}.
+     */
+    @Test
+    public void testGetSLDName() {
+        RasterFileHandler handler = new RasterFileHandler();
+
+        assertTrue(handler.getSLDName(null).compareTo("") == 0);
+
+        SLDData sldData = new SLDData(new StyleWrapper("workspace", "layer.sld"), "sldContents");
+        String sldName = handler.getSLDName(sldData);
+        assertTrue(sldName.compareTo("layer.sld") == 0);
+    }
+}
---
+++ modules/application/src/test/java/com/sldeditor/test/unit/extension/filesystem/file/vector/VectorFileHandlerTest.java
@@ -0,0 +1,148 @@
+/*
+ * SLD Editor - The Open Source Java SLD Editor
+ *
+ * Copyright (C) 2016, SCISYS UK Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.sldeditor.test.unit.extension.filesystem.file.vector;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.sldeditor.common.SLDDataInterface;
+import com.sldeditor.common.data.SLDData;
+import com.sldeditor.common.data.StyleWrapper;
+import com.sldeditor.datasource.extension.filesystem.node.file.FileTreeNode;
+import com.sldeditor.extension.filesystem.file.vector.VectorFileHandler;
+
+/**
+ * Unit test for VectorFileHandler class.
+ * <p>{@link com.sldeditor.extension.filesystem.file.vector.VectorFileHandler}
+ * 
+ * @author Robert Ward (SCISYS)
+ *
+ */
+public class VectorFileHandlerTest {
+
+    /**
+     * Test method for {@link com.sldeditor.extension.filesystem.file.vector.VectorFileHandler#getFileExtensionList()}.
+     */
+    @Test
+    public void testGetFileExtension() {
+        assertEquals(Arrays.asList("shp"), new VectorFileHandler().getFileExtensionList());
+    }
+
+    /**
+     * Test method for {@link com.sldeditor.extension.filesystem.file.vector.VectorFileHandler#isDataSource()}.
+     */
+    @Test
+    public void testIsDataSource() {
+        assertTrue(new VectorFileHandler().isDataSource());
+    }
+
+    /**
+     * Test method for {@link com.sldeditor.extension.filesystem.file.vector.VectorFileHandler#populate(com.sldeditor.common.filesystem.FileSystemInterface, javax.swing.tree.DefaultTreeModel, com.sldeditor.datasource.extension.filesystem.node.file.FileTreeNode)}.
+     */
+    @Test
+    public void testPopulate() {
+        assertFalse(new VectorFileHandler().populate(null, null, null));
+    }
+
+    /**
+     * Single file
+     * 
+     * Test method for {@link com.sldeditor.extension.filesystem.file.vector.VectorFileHandler#getSLDContents(com.sldeditor.common.NodeInterface)}.
+     */
+    @Test
+    public void testGetSLDContentsFile() {
+        assertNull(new VectorFileHandler().getSLDContents(null));
+
+        URL url = VectorFileHandlerTest.class.getResource("/point/sld");
+
+        File parent = null;
+        try {
+            parent = new File(url.toURI());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+
+        try {
+            FileTreeNode fileTreeNode = new FileTreeNode(parent, "point_attribute.sld");
+
+            VectorFileHandler handler = new VectorFileHandler();
+
+            List<SLDDataInterface> sldDataList = handler.getSLDContents(fileTreeNode);
+
+            assertNull(sldDataList);
+
+            // Try with valid vector file
+            FileTreeNode fileTreeNode2 = new FileTreeNode(parent, "point_attribute.shp");
+
+            sldDataList = handler.getSLDContents(fileTreeNode2);
+
+            assertTrue(sldDataList.isEmpty());
+
+            // Changes where the file is to be saved to
+            File saveFile = File.createTempFile("test", ".sld");
+            saveFile.deleteOnExit();
+
+            SLDData sldData = new SLDData(null, "");
+
+            sldData.setSLDFile(saveFile);
+
+            assertFalse(handler.save(null));
+            assertFalse(handler.save(sldData));
+        } catch (SecurityException e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+    }
+
+    /**
+     * Supply a folder name and retrieve all the sld files in it
+     * 
+     * Test method for {@link com.sldeditor.extension.filesystem.file.vector.VectorFileHandler#getSLDContents(com.sldeditor.common.NodeInterface)}.
+     */
+    @Test
+    public void testGetSLDName() {
+        VectorFileHandler handler = new VectorFileHandler();
+
+        assertTrue(handler.getSLDName(null).compareTo("") == 0);
+
+        SLDData sldData = new SLDData(new StyleWrapper("workspace", "layer.sld"), "sldContents");
+        String sldName = handler.getSLDName(sldData);
+        assertTrue(sldName.compareTo("layer.sld") == 0);
+    }
+}
