3fb8cc510f61c86c03edee998738f7b71402f2e3
Robert Ward <robert.ward@scisys.co.uk>
2017-03-05 13:02:19 +0000

    Datasource field order preserved after adding new field

--- modules/application/src/test/java/com/sldeditor/test/unit/datasource/impl/DataSourceImplTest.java
+++ modules/application/src/test/java/com/sldeditor/test/unit/datasource/impl/DataSourceImplTest.java
@@ -130,17 +130,7 @@ public class DataSourceImplTest {
         // Check fields extracted ok
         List<String> expectedFieldList = editorFile.getExpectedFieldList();
         assertTrue(expectedFieldList.size() == actualFieldnameList.size());
-
-        // Not assuming fields are in the same order
-        int count = 0;
-        for(String fieldName : actualFieldnameList)
-        {
-            if(expectedFieldList.contains(fieldName))
-            {
-                count ++;
-            }
-        }
-        assertTrue(expectedFieldList.size() == count);
+        assertEquals(expectedFieldList, actualFieldnameList);
 
         // Check for fields of certain types
         assertFalse(ds.getAttributes(Integer.class).isEmpty());
