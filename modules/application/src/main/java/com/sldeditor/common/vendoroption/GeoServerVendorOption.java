4b081292c509f819f8d1ccea786d14ac807e2d22
Rob Ward <robert.ward@scisys.co.uk>
2017-04-27 14:18:26 +0100

    Move Not Set to first in the list

--- modules/application/src/main/java/com/sldeditor/common/vendoroption/GeoServerVendorOption.java
+++ modules/application/src/main/java/com/sldeditor/common/vendoroption/GeoServerVendorOption.java
@@ -97,6 +97,9 @@ public class GeoServerVendorOption implements VendorOptionTypeInterface {
     public List<VersionData> getVersionList() {
         if (!listReversed) {
             Collections.reverse(versionList);
+            // Move the Not Set version is first in the list
+            VersionData notSet = versionList.remove(versionList.size() - 1);
+            versionList.add(0, notSet);
             listReversed = true;
         }
 
