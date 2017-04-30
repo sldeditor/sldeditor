f8097f686e2c801920241277af7042726d59a89f
Robert Ward <robert.ward@scisys.co.uk>
2017-04-30 10:46:29 +0100

    Replace sun.misc.Base64Encoder with apache.commons

---
+++ modules/application/src/main/java/com/sldeditor/common/property/EncryptedPropertiesInterface.java
@@ -0,0 +1,28 @@
+package com.sldeditor.common.property;
+
+public interface EncryptedPropertiesInterface {
+
+    /**
+     * Initialise.
+     *
+     * @param password the password
+     */
+    void initialise(String password);
+
+    /**
+     * Decrypt a string.
+     *
+     * @param str the str
+     * @return the string
+     */
+    String decrypt(String str);
+
+    /**
+     * Encrypt a string.
+     *
+     * @param str the str
+     * @return the string
+     */
+    String encrypt(String str);
+
+}
