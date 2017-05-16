361d2e479b88fbc09b2006d1375dfcbc2cf67c01
Unknown <robert.ward@scisys.co.uk>
2017-05-16 17:45:40 +0100

    Attempt

--- build/createdeb/createdeb.sh
+++ build/createdeb/createdeb.sh
@@ -1,20 +1,35 @@
 #!/bin/sh
 
-rm -rf debian
+pwd=$(pwd)
 
-mkdir debian
-echo "7" > debian/compat
+cd ../../../
+if [ ! -d pkgdeb ]; then
+mkdir pkgdeb
+cd pkgdeb
+git clone https://github.com/robward-scisys/sldeditor.git
+cd sldeditor
+rm -rf .git
+mvn clean install -DskipTests
+cd ..
+tar -zcvf sldeditor-0.7.5.tar.gz sldeditor
+else
+cd pkgdeb
+rm -rf sldeditor
+fi
 
-mkdir -p debian/source
-echo "3.0 (native)" > debian/source/format
+export DEBFULLNAME="Robert Ward"
+export DEBEMAIL="sldeditor.group@gmail.com"
+bzr whoami "$DEBFULLNAME '<'$DEBEMAIL'>'"
+#dh_make -i --copyright gpl3 -f ../sldeditor-0.7.5.tar.gz
+bzr dh-make sldeditor 0.7.5 sldeditor-0.7.5.tar.gz
+cd sldeditor
+cd debian
+rm *ex *EX README.*
+cd ..
+cp $pwd/control debian
+cp $pwd/sldeditor.install debian
+cp $pwd/copyright debian
 
-cp copyright debian/
-
-cp control debian/
-
-cp rules debian/
-chmod +x debian/rules
-
-cp changelog debian/
-
-cp ../../bin/SLDEditor.jar lib
+bzr add .
+bzr commit -m "Initial commit"
+bzr builddeb -- -us -uc
