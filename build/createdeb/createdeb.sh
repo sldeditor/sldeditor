de8256a5181a3c5b308bfb3ad4a9d8a915c3097e
Robert Ward <robert.ward@scisys.co.uk>
2017-05-19 15:42:01 +0100

    Merge remote-tracking branch 'origin/iss339-deb-package' into iss339-deb-package Removed jar

--- build/createdeb/createdeb.sh
+++ build/createdeb/createdeb.sh
@@ -44,8 +44,6 @@ cp $dirRunningFrom/control debian
 cp $dirRunningFrom/sldeditor.install debian
 cp $dirRunningFrom/copyright debian
 cp $dirRunningFrom/rules debian
-#cp $dirRunningFrom/SLDEditor.jar bin
-
 find debian/changelog -type f -exec sed -i 's/unstable/'$dist'/g' {} \;
 find debian/changelog -type f -exec sed -i 's/ (Closes: #nnnn)  <nnnn is the bug number of your ITP>//g' {} \;
 
