90b3fa9d129e457c00320ecfed7c8c4b3406c0be
Robert Ward <robert.ward@scisys.co.uk>
2017-06-21 16:03:23 +0100

    Updated to use ArcGIS 10.5 (#348)

--- modules/import/bin/generateSLD.bat
+++ modules/import/bin/generateSLD.bat
@@ -2,5 +2,5 @@ rem %1 = MXD file
 rem %2 = Output folder
 rem %3 = Output format, valid values are SLD, YSLD [optional, default is SLD]
 set INTERMEDIATE_FILE=%TEMP%\\intermediate.json
-"%AGSDESKTOPJAVA%"\java\jre\bin\java -Xss2m -Xmx512m -classpath "%AGSDESKTOPJAVA%\java\lib\arcobjects.jar";ImportMXD.jar com.sldeditor.importdata.esri.ImportMXD %1 %INTERMEDIATE_FILE%
+"%AGSDESKTOPJAVA%\java\jre\bin\java" -Xss2m -Xmx512m -classpath "%AGSDESKTOPJAVA%\java\lib\arcobjects.jar";ImportMXD.jar com.sldeditor.importdata.esri.ImportMXD %1 %INTERMEDIATE_FILE%
 "%JAVA_HOME%"\jre\bin\java -jar ExportSLD.jar %INTERMEDIATE_FILE% %2 %3
