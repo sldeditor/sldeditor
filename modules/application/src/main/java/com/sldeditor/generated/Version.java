a2760a76f08d32c73b2cdd5d362f9ab9b0fbba28
Unknown <robert.ward@scisys.co.uk>
2017-10-24 19:39:52 +0100

    Updated version 0.7.8-SNAPSHOT

--- modules/application/src/main/java/com/sldeditor/generated/Version.java
+++ modules/application/src/main/java/com/sldeditor/generated/Version.java
@@ -38,7 +38,7 @@ public class Version {
     private static final String APP_COPYRIGHT_YEAR = "2016";
 
     /** The version taken from the maven pom. */
-    public static String pomVersionString = "0.7.7";
+    public static String pomVersionString = "0.7.8-SNAPSHOT";
 
     /**
      * Gets the version number.
